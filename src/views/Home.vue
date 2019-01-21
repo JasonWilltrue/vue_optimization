4890ec4abbef520cd01c2f07aec1b81be9ffd8bb
lovewcc <148373644@qq.com>
2019-01-21 11:31:55 +0800

    安装与初始化

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <svg-icon icon-class="Addheart" class="logo"  />
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
@@ -16,3 +17,9 @@ export default {
   }
 }
 </script>
+
+<style media="screen">
+    .logo{
+      font-size: 10em;
+    }
+</style>
---
+++ vue.config.js
@@ -0,0 +1,39 @@
+const path = require('path')
+
+function resolve(dir) {
+  return path.join(__dirname, './', dir)
+}
+
+module.exports = {
+  resolve: {
+    // 自动补全的扩展名
+    extensions: ['.js', '.vue', '.json'],
+    // 默认路径代理
+    // 例如 import Vue from 'vue'，会自动到 'vue/dist/vue.common.js'中寻找
+    alias: {
+      '@': resolve('src'),
+      '@config': resolve('config'),
+      'vue$': 'vue/dist/vue.common.js'
+    }
+  },
+  chainWebpack: config => {
+    // svg loader
+    const svgRule = config.module.rule('svg') // 找到svg-loader
+    svgRule.uses.clear() // 清除已有的loader, 如果不这样做会添加在此loader之后
+    svgRule.exclude.add(/node_modules/) // 正则匹配排除node_modules目录
+    svgRule // 添加svg新的loader处理
+      .test(/\.svg$/)
+      .use('svg-sprite-loader')
+      .loader('svg-sprite-loader')
+      .options({
+        symbolId: 'icon-[name]'
+      })
+
+    // 修改images loader 添加svg处理
+    const imagesRule = config.module.rule('images')
+    imagesRule.exclude.add(resolve('src/icons'))
+    config.module
+      .rule('images')
+      .test(/\.(png|jpe?g|gif|svg)(\?.*)?$/)
+  }
+}
