c38b8ddc68412ed3f9d647ee31176a4ce44a6f8f
lovewcc <148373644@qq.com>
2019-01-21 12:14:12 +0800

    svg-icon优化

--- src/icons/index.js
+++ src/icons/index.js
@@ -4,6 +4,7 @@ import SvgIcon from '@/components/SvgIcon.vue' // svg组件
 // 全局注册
 Vue.component('svg-icon', SvgIcon)
 
+
 const requireAll = requireContext => requireContext.keys().map(requireContext)
 const req = require.context('./svg', false, /\.svg$/)
 requireAll(req)
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <svg-icon icon-class="Addheart" class="logo"  />
-    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+    <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
